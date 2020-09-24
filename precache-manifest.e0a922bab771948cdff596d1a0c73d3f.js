535450e29b6c539caee268913f4f26875a7fb648
junbeoman <wnsqja8744@gmail.com>
2020-09-24 15:14:19 +0900

    Updates

---
+++ precache-manifest.e0a922bab771948cdff596d1a0c73d3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bbedd78e2f8aa7f4acb46f87bac3bd",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "4b86efffca5df400aeb7",
+    "url": "/nwitter/static/js/2.513c38bd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.513c38bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c171f9d6f0506ed1ead",
+    "url": "/nwitter/static/js/main.a0c4ee3e.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
